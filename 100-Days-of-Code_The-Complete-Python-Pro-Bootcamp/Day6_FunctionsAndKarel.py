b7f1e5d764ac8d5e99c5a18fd0d3dae95f00aba7
Doug-LT <dugbentley@yahoo.com>
2025-09-10 00:12:01 -0600

    Day 6 Complete

--- 100-Days-of-Code_The-Complete-Python-Pro-Bootcamp/Day6_FunctionsAndKarel.py
+++ 100-Days-of-Code_The-Complete-Python-Pro-Bootcamp/Day6_FunctionsAndKarel.py
@@ -53,27 +53,43 @@
 #         move()
 
 # Unknown height
-def turn_right():
-    for turn in range(3):
-        turn_left()
+# def turn_right():
+#     for turn in range(3):
+#         turn_left()
+#
+# def jump_hurdle():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     for right in range(2):
+#         turn_right()
+#         move()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+#
+# while not at_goal():
+#     if wall_in_front():
+#         jump_hurdle()
+#     else:
+#         move()
 
-
-def jump_hurdle():
-    count = 0
-    turn_left()
-    while wall_on_right():
-        move()
-        count += count
-    turn_right()
-    move()
-    turn_right()
-    for move in range(count):
-        move()
-    turn_left()
-
-
-while not at_goal():
-    if wall_in_front():
-        jump_hurdle()
-    else:
-        move()
+# Maze
+# If the right is clear turn right and move
+# If ahead is clear move
+# if it can’t turn right, or move forwar turn left
+# Infinite loop can occur at rows 4 & 5 and columns 2 & 3
+# def turn_right():
+#     for turn in range(3):
+#         turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+#         if at_goal():
+#             move()
