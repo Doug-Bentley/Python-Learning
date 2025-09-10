0bf3fea383930a454b35e6cfe720ec82f7a54615
Doug-Bentley <dugbentley@yahoo.com>
2025-09-09 20:19:49 -0600

    Day 6 of 100...

---
+++ 100-Days-of-Code_The-Complete-Python-Pro-Bootcamp/Day6_FunctionsAndKarel.py
@@ -0,0 +1,54 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# Hurdles
+# def turn_right():
+#     for turn in range(0, 3):
+#         turn_left()
+#
+# def jump_hurdle():
+#     move()
+#     turn_left()
+#     move()
+#     for right in range(0, 2):
+#         turn_right()
+#         move()
+#     turn_left()
+#
+# for jump in range(6):
+#     jump_hurdle()
+
+# Unknown number of hurdles
+# def turn_right():
+#     for turn in range(0, 3):
+#         turn_left()
+#
+# def jump_hurdle():
+#     move()
+#     turn_left()
+#     move()
+#     for right in range(0, 2):
+#         turn_right()
+#         move()
+#     turn_left()
+#
+# while at_goal() == False:
+#     jump_hurdle()
+
+# Unknown track
+# def turn_right():
+#     for turn in range(3):
+#         turn_left()
+#
+# def jump_hurdle():
+#     turn_left()
+#     move()
+#     for right in range(2):
+#         turn_right()
+#         move()
+#     turn_left()
+#
+# while not at_goal():
+#     if wall_in_front():
+#         jump_hurdle()
+#     else:
+#         move()
+
