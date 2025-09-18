eac98c75de4c3f25f8db1402958ca6f93641eafb
Doug-Bentley <dugbentley@yahoo.com>
2025-09-17 21:51:39 -0600

    Day 7 ......

--- 100-Days-of-Code_The-Complete-Python-Pro-Bootcamp/Day7_Hangman.py
+++ 100-Days-of-Code_The-Complete-Python-Pro-Bootcamp/Day7_Hangman.py
@@ -7,13 +7,26 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 word_length = len(chosen_word)
 print(word_length)
-placeholder = ""
-# for i in range(word_length):
-#     placeholder[i] = "_"
-guess = input("\nGuess a letter: ").lower()
-for letter in chosen_word:
-    if letter == guess:
-        placeholder += letter
-    else:
-        placeholder += "_"
-print(placeholder)
+word_solve = ""
+for position in range(word_length):
+    word_solve += "_"
+print(word_solve)
+count = 0
+while word_solve != chosen_word and count < 6:
+    correct = False
+    guess = input("\nGuess a letter: ").lower()
+    # Replace letter in placeholder to guessed letter if guessed correctly
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            word_solve = word_solve[:position] + letter + word_solve[position + 1:]
+            correct = True
+    if not correct:
+        count += 1
+        print(f"You have {6 - count} attempts left.")
+    print(word_solve)
+if word_solve == chosen_word:
+    print("You win!")
+else:
+    print("You lose.")
+    print(f"The word was {chosen_word}.")
